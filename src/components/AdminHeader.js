09d8a20d26178718b3bd2bb0f36d79ed492a9e59
Aaron Skinner <aaroncurtisskinner@gmail.com>
2022-11-08 21:29:09 -0500

    changes made to Admin Header, Registration list,

--- src/components/AdminHeader.js
+++ src/components/AdminHeader.js
@@ -18,54 +18,67 @@ export default function UserHeader(){
 
     return (
         <Fragment>
-            <nav className="navbar navbar-expand-lg navbar-dar bg-dark">
-                <a className="navbar-brand" href="#">
-                    Social Network
-                </a>
-                <button 
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon">
+            {/* <!-- Navbar --> */}
+<nav class="navbar navbar-expand-lg bg-secondary navbar-dark ">
+  {/* <!-- Container wrapper --> */}
+  <div class="container-fluid">
 
-                    </span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">
-                                Welcome <span className="sr-only"></span>
-                                Admin
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/registrationlist" className="nav-link">
-                                Registration Management
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="articlelist" className="nav-link">
-                                Article Management
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="newslist" className="nav-link">
-                                News Management
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="stafflist" className="nav-link">
-                                staff Management
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+    {/* <!-- Navbar brand --> */}
+    <a class="navbar-brand" href="#">Social Network</a>
+
+    {/* <!-- Toggle button --> */}
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    {/* <!-- Collapsible wrapper --> */}
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        {/* <!-- Greeting --> */}
+        <h5>Welcome {username}</h5>
+        {/* <!-- Dropdown --> */}
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+            Management
+          </a>
+          {/* <!-- Dropdown menu --> */}
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li>
+                <a class="dropdown-item" href="/registrationlist">Registration Management</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="/articlelist">Article Management</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="/newslist">News Management</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="/stafflist">Staff Management</a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      {/* <!-- Icons --> */}
+      <ul class="navbar-nav d-flex flex-row me-1">
+        <li class="nav-item me-3 me-lg-0">
+          <a class="nav-link" href="#"><i class="fa-solid fa-right-from-bracket"></i></a>
+        </li>
+        <li class="nav-item me-3 me-lg-0">
+          <a class="nav-link" href="https://github.com/OrneryLion"><i class="fab fa-github"></i></a>
+        </li>
+      </ul>
+
+      {/* <!-- Search --> */}
+      <form class="w-auto">
+        <input type="search" class="form-control" placeholder="Type query" aria-label="Search"/>
+      </form>
+
+    </div>
+  </div>
+  {/* <!-- Container wrapper --> */}
+</nav>
+{/* <!-- Navbar --> */}
         </Fragment>
     )
 }
